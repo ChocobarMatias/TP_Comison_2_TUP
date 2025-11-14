3018cd8669b1f3a422e390d5c6bbd033b36ee955
marcosdgr <marcos.ciruja22@gmail.com>
2025-11-14 18:59:39 -0300

    confiraciones de api y zustand

--- Trabajo Grupales/Grupo N° 2 - TP 6/Frontend/src/Store/UseAuthStore.js
+++ Trabajo Grupales/Grupo N° 2 - TP 6/Frontend/src/Store/UseAuthStore.js
@@ -1,3 +1,8 @@
-const useAuthStore = () => {};
-
-export default useAuthStore;
+import create from 'zustand';
+export const useAuthStore = create((set) => ({
+  user: null,
+  token: null,
+  setUser: (user) => set({ user }),
+  setToken: (token) => set({ token }),
+  logout: () => set({ user: null, token: null }),
+}));
