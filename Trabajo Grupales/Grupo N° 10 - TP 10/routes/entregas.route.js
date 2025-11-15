5b6774751f8bf4a47736003bb91bbc26005983ec
unknown <facubazan992009@gmail.com>
2025-11-15 16:57:08 -0300

    elimino carpetas fanstasmas y agrego rutas

---
+++ Trabajo Grupales/Grupo N° 10 - TP 10/routes/entregas.route.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import {
+  deleteEntrega,
+  getEntregas,
+  getOneEntrega,
+  postEntrega,
+  putEntrega,
+} from '../controllers/entregas.controller';
+
+const route = Router();
+
+route.get('/', getEntregas);
+route.get('/:id', getOneEntrega);
+route.post('/', postEntrega);
+route.put('/:id', putEntrega);
+route.delete('/:id', deleteEntrega);
+
+export default route;
