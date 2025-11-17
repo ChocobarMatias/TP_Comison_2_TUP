7bda2b51a770edc3c66e35a1b1f2a87ae991451b
unknown <facubazan992009@gmail.com>
2025-11-16 23:05:37 -0300

    traigo ultimos cambios y modulo estructura del tp2 que habia quedado pendiente

--- Trabajo Grupales/Grupo N° 10 - TP 8/Registro de Actividades Culturales en una Municipalidad/back-end/src/routes/auth.routes.js
+++ Trabajo Grupales/Grupo N° 10 - TP 8/Registro de Actividades Culturales en una Municipalidad/back-end/src/routes/auth.routes.js
@@ -1,4 +1,5 @@
-const router = require(`express`);
+const express = require(`express`);
+const router = express.Router()
 const {solicitarReset,resetPassword} = require("../controllers/auth.controller");
 
 router.post(`solicitar-reset`, solicitarReset);
